05f6f6f069878cd3362ca26a94b366ed75fc2a20
Jimmy Thai <jimmy@zephlit.com>
2016-08-06 15:05:47 -0400

    Convert rails to middleman

---
+++ helpers/page.rb
@@ -0,0 +1,44 @@
+module Page
+  def pages
+    [
+      :wayfind,
+      :onespark,
+      :resumebot,
+      :ignite,
+      :txtout,
+      :aiga,
+      :matchstiq,
+      :asps,
+      :catalyst,
+      :oppify,
+      :trucknseek,
+      :combofonts,
+      :spinnaker,
+      :metrology,
+      :traverse,
+      :bistro23,
+      :rumble,
+      :browndog
+    ]
+  end
+
+  def page_names
+    pages.map(&:to_s)
+  end
+
+  def next_page_of(name)
+    index = pages.index(name.to_sym)
+    return nil unless index.present?
+
+    next_index = (index + 1) % pages.length
+    pages[next_index].to_s
+  end
+
+  def prev_page_of(name)
+    index = pages.index(name.to_sym)
+    return nil unless index.present?
+
+    prev_index = (index - 1) % pages.length
+    pages[prev_index].to_s
+  end
+end
--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,58 @@
-//= require_tree .
+//= require vendor/jquery-3.1.0.min
+//= require vendor/modernizr.min
+//= require vendor/selectivizr.min
+//= require vendor/smoothState
+//= require vendor/windows.min
+
+$(document).ready(function() {
+  var hookEvents = function() {
+    $('#nav-icon').click(function() {
+       $('.overlay').slideToggle( 'show' );
+       $(this).toggleClass('open');
+    });
+
+    if ($('.div-toggler').length > 0) {
+
+      $('select.div-toggler').change(function() {
+        var target = $(this).data('target');
+        $(target).children().addClass('hide');
+
+        var show = $("option:selected", this).data('show');
+        $(show).removeClass('hide');
+      });
+    }
+  }
+
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+        hookEvents();
+      }
+    },
+    onBefore: function($currentTarget, $container) {
+      $('#nav-icon').off('click');
+    }
+  };
+
+  window.smoothState = $('#main').smoothState(options).data('smoothState');
+  hookEvents();
+});
